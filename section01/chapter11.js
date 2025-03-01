2f128497d567167a9fb4ce896544a76b70bb418f
Hanjo <144607552+Hanjo35@users.noreply.github.com>
2025-03-02 02:13:07 +0900

    13강까지 중간백업

---
+++ section01/chapter11.js
@@ -0,0 +1,23 @@
+// 함수
+
+function getArea (width, height) {
+    let area = width * height;
+
+    function another() {
+        console.log("another");
+    }
+
+    another();
+    return area;
+}
+
+let area1 = getArea(10, 20);
+console.log(area1);
+
+let area2 = getArea(20, 30);
+console.log(area2);
+
+getArea(120, 200);
+
+// 자바스크립트는 함수 호출식을 위로 올려버려도 정상작동됨.
+// => 자바스크립트에는 호이스팅(끌어올리기)이 가능하기 때문
---
+++ section01/chapter12.js
@@ -0,0 +1,25 @@
+// 1. 함수 표현식
+
+function funcA() {
+    // console.log("funcA");
+}
+
+let varA = funcA;   
+varA();  // 함수 자체를 변수에 담아두기 가능 
+
+
+let varB = function() { // 익명함수 => 함수 이름 없이 변수에 바로 담아 선언
+    // console.log("funcB");
+}
+
+varB(); // 값으로서 선언이 되는 것이기 떄문에 funcA 와 달리 호이스팅 되지 않음
+
+// 2. 화살표 함수
+let varC = () => 1;
+let varD = (value) => value + 1; 
+let varE = (value) => {
+    console.log(value);
+    return value + 1; // 추가적인 작업 필요하면 중괄호로 감싸서 사용
+}
+
+console.log(varD(10)); 
