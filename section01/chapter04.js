ddbc560ebae273ab11a6e435d1c054258a75d72e
Hanjo <hanjo0305@gmail.com>
2025-03-05 00:32:37 +0900

    Update chapter04.js

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -12,7 +12,18 @@ const birth ="1999.03.05";
 // 3. 변수 명명 규칙 (네이밍 규칙)
 // 3-1. $, _ 제외한 특수문자 사용 불가
 // 3-2. 변수의 이름은 숫자로 시작 불가 (ex. 1age, 2name)
+let name1;
+let $2name;
+
 // 3-3. 예약어 사용 불가 (ex. let, const, function) 
 
 // 4. 변수 명명 가이드
+ let a = 1;
+ let b = 2;
+ let c = a - b;
+
+ let salesCount = 1;
+ let refundCount = 2;
+ let totalSalesCount = salesCount - refundCount;
+
  
